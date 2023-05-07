1aef732992eaadc9e09a5dd3789104f73d3cef0d
WebCONuz <muxammadi0799@gmail.com>
2023-05-07 19:45:47 +0500

    Fixed 'Expited token' error

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,8 @@ import AdminLayout from "../layouts/AdminLayout.vue";
 import LoginAdmin from "../layouts/LoginAdmin.vue";
 import NotFound from "../layouts/NotFound.vue";
 
+import { toast } from "vue3-toastify";
+
 // Routing
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,27 +57,34 @@ const router = createRouter({
 // Check Token
 import axios from "@/Service/axios";
 router.beforeEach(async (to, from) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    const result = await axios.post("/admin/check-token", { token });
-    if (result?.data?.isValid) {
-      localStorage.setItem("id", result.data.data.id);
-      localStorage.setItem("role", result.data.data.is_creator ? 1 : 2);
-      localStorage.setItem("username", result.data.data.user_name);
-      return true;
-    } else {
+  try {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const result = await axios.post("/admin/check-token", { token });
+      if (result?.data?.isValid) {
+        localStorage.setItem("id", result.data.data.id);
+        localStorage.setItem("role", result.data.data.is_creator ? 1 : 2);
+        localStorage.setItem("username", result.data.data.user_name);
+        return true;
+      }
+    } else if (!token && to.path !== "/") {
       localStorage.removeItem("username");
       localStorage.removeItem("role");
       localStorage.removeItem("id");
       localStorage.removeItem("token");
       return "/";
     }
-  } else if (!token && to.path !== "/") {
-    localStorage.removeItem("username");
-    localStorage.removeItem("role");
-    localStorage.removeItem("id");
-    localStorage.removeItem("token");
-    return "/";
+  } catch (err) {
+    if (!err.response.data.isValid) {
+      toast.error(err.response.data.error);
+      localStorage.removeItem("username");
+      localStorage.removeItem("role");
+      localStorage.removeItem("id");
+      localStorage.removeItem("token");
+      return "/";
+    } else {
+      console.log(err);
+    }
   }
 });
 
--- src/service/axios.js
+++ src/service/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-axios.defaults.baseURL = "http://146.190.207.16:5050";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 axios.defaults.headers.common["Authorization"] =
   "Bearer " + localStorage.getItem("token");
 
